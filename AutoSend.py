948e5236ae2d5736d4df60afa56858a35c91f138
Hayden Kirschbaum <hlkbaum@berkeley.edu>
2021-10-18 20:58:07 -0700

    basic functionality for multiple users

--- AutoSend.py
+++ AutoSend.py
@@ -7,24 +7,28 @@ from email.message import EmailMessage
 from datetime import datetime
 import parsedatetime as pdt
 
-SEND = False
+SEND = True
 
 def auto_reply():
 
 
-    commands = ['add', 'clear', 'show','reminder']
+    commands = ['add', 'clear', 'show','reminder', 'delete']
     user,pswd = 'lifeupdate.msg@gmail.com', 'rvqeiqxcoftltxgg'
 
     with MailBox('imap.gmail.com').login(user, pswd) as mailbox:
-        texts = [att[0].payload for att in [msg.attachments for msg in mailbox.fetch(A(seen=False), mark_seen=False)]]
-        senders = [msg.from_ for msg in mailbox.fetch(A(seen=False), mark_seen=False)]
+        try:
+            texts = [att[0].payload.decode('utf-8') for att in [msg.attachments for msg in mailbox.fetch(A(seen=False), mark_seen=False)]]
+        except:
+            texts = [msg.text.replace("\r\n",'') for msg in mailbox.fetch(A(seen=False), mark_seen=False)]
+        senders = [msg.from_ for msg in mailbox.fetch(A(seen=False), mark_seen=True)]
         #-------No New Messages will end the program-------
     if len(texts) == 0:
         return False
 
-
+    print(texts)
+    print(senders)
     # ----- now for the meat of it -------
-    for message,sender in zip([t.decode('utf-8') for t in texts],[s.split('@')[0] for s in senders]):
+    for message,sender in zip(texts,[s.split('@')[0] for s in senders]):
         exists = os.path.exists(f'users/{sender}')
         if not exists:
             # Create a new directory because it does not exist
@@ -41,7 +45,10 @@ def auto_reply():
         response = globals()[cmd](sender,list_name,content)
         print('response: ',response)
         # send message
-
+        if response is not None:
+            SEND = True
+        else:
+            SEND = False
 
         if SEND:
             msg = EmailMessage()
@@ -66,7 +73,7 @@ def add(directory, list_name,content):
     with open(f'users/{directory}/{list_name.lower()}.txt','a+') as file:
         file.write(f'\n{content}')
         file.close()
-    return None
+    return show(directory,list_name)
 
 def clear(directory,list_name,*args):
     with open(f'users/{directory}/{list_name.lower()}.txt', 'w+') as file:
@@ -74,11 +81,16 @@ def clear(directory,list_name,*args):
         file.close()
     return None
 
+def delete(directory,list_name,*args):
+    if os.path.exists(f'users/{directory}/{list_name.lower()}.txt'):
+        os.remove(f'users/{directory}/{list_name.lower()}.txt')
+    return None
+
 def show(directory,list_name,*args):
     with open(f'users/{directory}/{list_name.lower()}.txt', 'r') as file:
         msg = file.read()
         file.close()
-    return msg
+    return f'{list_name.upper()} LIST:{msg}'
 
 def reminder(directory,command,phrase):
     cal = pdt.Calendar()
@@ -102,4 +114,4 @@ def reminder(directory,command,phrase):
 
 
 
-print(auto_reply())
+auto_reply()
